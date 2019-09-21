3ad7ab6eda81c8e4873ff662c3d26b0baffc359a
rfiestas <rfiestas@gmail.com>
2019-09-21 18:11:48 +0200

    Adding schema tests

--- tumblr/schema_tumblr_post_test.go
+++ tumblr/schema_tumblr_post_test.go
@@ -1,3 +1,8 @@
 package tumblr
 
+import "regexp"
+
 var TestBlog = "terraform-provider-for-test"
+
+var TestError404NotFound = regexp.MustCompile("404 Not Found")
+var TestMustBeAssigned = regexp.MustCompile("must be assigned")
